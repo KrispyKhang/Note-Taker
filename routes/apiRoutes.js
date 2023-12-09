15ce0b7e8974a54f106d932dcc5878cc6d59e016
Khang Tran <creamybanana@Khang.invcorp.invensense.com>
2023-12-08 19:44:51 -0800

    added a GET to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,25 @@
 const router = require('express').Router();
+const fs = require('fs');
 
 
+
+router.get('/notes', (req, res) => {
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) {
+        // Handle the error, e.g., send an error response
+        res.status(500).json({ error: 'Unable to read the file.' });
+        return;
+      }
+  
+      try {
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
+      } catch (parseError) {
+        // Handle JSON parsing error, e.g., send an error response
+        res.status(500).json({ error: 'Invalid JSON data.' });
+      }
+    });
+  });
+  
 
 module.exports = router;
