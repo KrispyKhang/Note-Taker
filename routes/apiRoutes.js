8d1498e511aafb3291c74f3545271626f14aa09f
Khang Tran <creamybanana@Khang.invcorp.invensense.com>
2023-12-08 20:18:41 -0800

    added POST

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs');
+const {v4} = require('uuid');
 
 
 
@@ -16,10 +17,47 @@ router.get('/notes', (req, res) => {
         res.json(jsonData);
       } catch (parseError) {
         // Handle JSON parsing error, e.g., send an error response
-        res.status(500).json({ error: 'Invalid JSON data.' });
+        res.status(500).json({ error: 'Invalid JSONNN data.' });
       }
     });
   });
+
+
+  router.post('/notes', (req, res) => {
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) {
+        // Handle the error, e.g., send an error response
+        res.status(500).json({ error: 'Unable to read the file.' });
+        return;
+      }
   
+      try {
+        const jsonData = JSON.parse(data);
+        const newNote = {...req.body, id: v4()};
+        jsonData.push(newNote);
+
+        fs.writeFile('db/db.json', JSON.stringify(jsonData), 'utf-8', (err) => {
+            if (err) {
+              console.error('Error writing to file:', err);
+              return;
+            }
+            console.log('Data has been written to the file.');
+          });
+
+        res.json(jsonData);
+      } catch (parseError) {
+        // Handle JSON parsing error, e.g., send an error response
+        res.status(500).json({ error: 'Invalid JSON data.' });
+      }
+    });
+
+    
+
+    
+  });
+
+  
+
+
 
 module.exports = router;
